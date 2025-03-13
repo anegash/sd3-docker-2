5e52ae72f8f06b3ffdd635a26b7eb6d2aad98fe5
Anteneh Negash <antenehnegash@gmail.com>
2025-03-13 22:10:39 +0300

    runpod storage mount location change

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ def load_model():
         raise ValueError("HuggingFace token not found! Set HF_TOKEN environment variable.")
 
     model_path = snapshot_download(
-        repo_id="stabilityai/stable-diffusion-3.5-large-turbo",
-        local_dir="/data/models",  # Persistent volume mount point
+        repo_id="stabilityai/stable-diffusion-3.5-large",
+        local_dir="/runpod-volume/models",  # Persistent volume mount point
         token=HF_TOKEN,
         local_dir_use_symlinks=False,
         resume_download=True
